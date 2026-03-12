3b83ff326bebc48d56bb6df6efdb513bd5137ee6
Flit <8645405+FlitPix@users.noreply.github.com>
2026-03-12 10:52:11 -0400

    show pokemon to be sent out by opponent in switch mode

--- src/party_menu.c
+++ src/party_menu.c
@@ -2490,10 +2490,29 @@ void DisplayPartyMenuStdMessage(u32 stringId)
 
         if (stringId == PARTY_MSG_CHOOSE_MON)
         {
+            u8 enemyNextMonID = *(gBattleStruct->monToSwitchIntoId + B_SIDE_OPPONENT);
+            u16 species = GetMonData(&gEnemyParty[enemyNextMonID], MON_DATA_SPECIES);
+
             if (sPartyMenuInternal->chooseHalf)
                 stringId = PARTY_MSG_CHOOSE_MON_AND_CONFIRM;
             else if (!ShouldUseChooseMonText())
                 stringId = PARTY_MSG_CHOOSE_MON_OR_CANCEL;
+
+            else if (gMain.inBattle)
+            {
+                // check if the opponent is about to send out a new pokemon
+                if (species >= NUM_SPECIES || species == SPECIES_NONE)
+                {
+                    // check if any opposing pokemon are on the field
+                    species = gBattleMons[B_SIDE_OPPONENT].species;
+                    if (species >= NUM_SPECIES || species == SPECIES_NONE || gBattleTypeFlags & BATTLE_TYPE_DOUBLE)
+                        stringId = PARTY_MSG_CHOOSE_MON_2; // no species on other side, use default text
+                }
+                if (stringId == PARTY_MSG_CHOOSE_MON)
+                    StringCopy(gStringVar2, gSpeciesNames[species]);
+            }
+            else
+                stringId = PARTY_MSG_CHOOSE_MON_2;
         }
         DrawStdFrameWithCustomTileAndPalette(*windowPtr, FALSE, 0x4F, 13);
         StringExpandPlaceholders(gStringVar4, sActionStringTable[stringId]);
