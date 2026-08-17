d1fa6b208358e3babaf853816075ec3795493254
Flit <8645405+FlitPix@users.noreply.github.com>
2026-08-17 11:39:41 -0400

    add menu graphics definition for pokenav

--- src/pokenav_menu_handler_gfx.c
+++ src/pokenav_menu_handler_gfx.c
@@ -217,6 +217,17 @@ struct
             sOptionsLabelGfx_SwitchOff
         }
     },
+    [POKENAV_MENU_TYPE_UNLOCK_RIBBONS] =
+    {
+        .yStart = 42,
+        .deltaY = 20,
+        .gfx = {
+            sOptionsLabelGfx_RegionMap,
+            sOptionsLabelGfx_Condition,
+            sOptionsLabelGfx_Ribbons,
+            sOptionsLabelGfx_SwitchOff
+        }
+    },
     [POKENAV_MENU_TYPE_UNLOCK_MC_RIBBONS] =
     {
         .yStart = 42,
