75395eaefabf068715e53f11376b0b4713e737a7
Flit <8645405+FlitPix@users.noreply.github.com>
2026-08-17 15:11:51 -0400

    use default pokenav menu type as before

--- src/pokenav_menu_handler.c
+++ src/pokenav_menu_handler.c
@@ -92,7 +92,7 @@ static const u8 sMenuItems[][MAX_POKENAV_MENUITEMS] =
 
 static u8 GetPokenavMainMenuType(void)
 {
-    u8 menuType = POKENAV_MENU_TYPE_UNLOCK_RIBBONS;
+    u8 menuType = POKENAV_MENU_TYPE_DEFAULT;
 
     if (FlagGet(FLAG_ADDED_MATCH_CALL_TO_POKENAV))
     {
