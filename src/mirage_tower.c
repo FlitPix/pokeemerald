b0e408f9010f56294633d1ea811c61d6c4ee0584
Flit <8645405+FlitPix@users.noreply.github.com>
2026-08-16 23:12:48 -0400

    make mirage tower always visible if not yet completed

--- src/mirage_tower.c
+++ src/mirage_tower.c
@@ -314,8 +314,8 @@ void ClearMirageTowerPulseBlendEffect(void)
 
 void SetMirageTowerVisibility(void)
 {
-    u16 rand;
-    bool8 visible;
+    //u16 rand;
+    //bool8 visible;
 
     if (VarGet(VAR_MIRAGE_TOWER_STATE))
     {
@@ -324,19 +324,22 @@ void SetMirageTowerVisibility(void)
         return;
     }
 
-    rand = Random();
-    visible = rand & 1;
-    if (FlagGet(FLAG_FORCE_MIRAGE_TOWER_VISIBLE) == TRUE)
-        visible = TRUE;
+    // Mirage Tower is always visible if not completed
+    FlagSet(FLAG_MIRAGE_TOWER_VISIBLE);
 
-    if (visible)
-    {
-        FlagSet(FLAG_MIRAGE_TOWER_VISIBLE);
-        TryStartMirageTowerPulseBlendEffect();
-        return;
-    }
+    //rand = Random();
+    //visible = rand & 1;
+    //if (FlagGet(FLAG_FORCE_MIRAGE_TOWER_VISIBLE) == TRUE)
+    //    visible = TRUE;
 
-    FlagClear(FLAG_MIRAGE_TOWER_VISIBLE);
+    //if (visible)
+    //{
+    //    FlagSet(FLAG_MIRAGE_TOWER_VISIBLE);
+    //    TryStartMirageTowerPulseBlendEffect();
+    //    return;
+    //}
+
+    //FlagClear(FLAG_MIRAGE_TOWER_VISIBLE);
 }
 
 void StartPlayerDescendMirageTower(void)
