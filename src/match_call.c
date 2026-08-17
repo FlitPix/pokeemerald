f3e2d5d458daadda828448e119b6b4053f23fa39
Flit <8645405+FlitPix@users.noreply.github.com>
2026-08-16 23:08:59 -0400

    don't receive random match calls

--- src/match_call.c
+++ src/match_call.c
@@ -1155,6 +1155,8 @@ static u32 GetActiveMatchCallTrainerId(u32 activeMatchCallId)
 */
 bool32 TryStartMatchCall(void)
 {
+    return FALSE;
+    
     if (FlagGet(FLAG_HAS_MATCH_CALL)
         && UpdateMatchCallStepCounter()
         && UpdateMatchCallMinutesCounter()
